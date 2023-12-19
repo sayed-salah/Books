a3e71c15d2997e5f69cf6e10786d9b37162f16b5
sayed-sala7 <sayed.salah@elsewedy.com>
2023-12-19 16:08:31 +0200

    best seller list view scroll

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:books_app/core/utils/styles.dart';
 import 'package:books_app/features/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'best_seller_list_view.dart';
 import 'best_seller_list_view_item.dart';
 import 'feature_list_view.dart';
 
@@ -9,23 +10,60 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 50,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                  ),
+                  child: CustomAppBar()),
+              FeaturedBooksListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Newset Books',
+                  style: Styles.textStyle18,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          Text('Best Seller', style: Styles.textStyle18),
-          SizedBox(
-            height: 20,
+        ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          BookListViewItem()
-        ],
-      ),
+        )
+      ],
     );
+
+    // return const Padding(
+    //   padding: EdgeInsets.symmetric(horizontal: 30),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       CustomAppBar(),
+    //       FeaturedBooksListView(),
+    //       SizedBox(
+    //         height: 50,
+    //       ),
+    //       Text('Best Seller', style: Styles.textStyle18),
+    //       SizedBox(
+    //         height: 20,
+    //       ),
+    //       BookListViewItem()
+    //     ],
+    //   ),
+    // );
   }
 }
