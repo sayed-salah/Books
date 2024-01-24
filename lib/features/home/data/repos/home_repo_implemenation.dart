f3ded32af39e48a9025d44eae1d27b3ae9b3bdaf
sayed-sala7 <sayed.salah@elsewedy.com>
2024-01-24 12:00:18 +0200

    create featured books state

--- lib/features/home/data/repos/home_repo_implemenation.dart
+++ lib/features/home/data/repos/home_repo_implemenation.dart
@@ -1,7 +1,7 @@
 import 'package:books_app/core/error/failure.dart';
 import 'package:books_app/core/utils/api_service.dart';
 import 'package:books_app/features/home/data/models/book_model/book_model.dart';
-import 'package:books_app/features/home/data/models/book_model/repos/home_repo.dart';
+import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
@@ -41,8 +41,27 @@ class HomeRepoImplementation extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
