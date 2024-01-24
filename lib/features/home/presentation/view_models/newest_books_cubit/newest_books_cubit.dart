e9b86f03f582bc6d6b48cfcca3269d3eccb1b554
sayed-sala7 <sayed.salah@elsewedy.com>
2024-01-24 12:08:43 +0200

    add newset cubit

---
+++ lib/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:books_app/features/home/data/models/book_model/book_model.dart';
+import 'package:books_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
+}
